1eb3f7b364cdad063488ded8d0102b6cb17c2071
florayuyuun123 <fyuyuun@gmail.com>
2025-03-16 09:25:43 +0100

    last commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,21 @@
 // app/src/index.js
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Simple route to confirm the application is running
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.send('Hello, Kubernetes DevOps Pipeline!');
 });
 
-// Bind to 0.0.0.0 to allow external access
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://0.0.0.0:${port}`);
 });
